071bd3309228f0e243b38dc4b99968c80c63b731
jesse <jsoldat@hotmail.com>
2017-01-13 12:10:05 +0700

    can fetch bookmarks from local storage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log('main.js');
+// console.log('main.js');
 
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
@@ -8,12 +8,48 @@ function saveBookmark(e){
 
 	var siteName = document.getElementById('siteName').value;
 	var siteUrl = document.getElementById('siteUrl').value;
-	console.log(siteName);
-
+	
 	var bookmark = {
 		name: siteName,
 		url: siteUrl
 	}
-	console.log(bookmark);
 
+	// console.log(bookmark);
+
+	if(localStorage.getItem('bookmarks') === null){
+		
+		var bookmarks = [];
+
+		// bookmarks.push(bookmark);
+
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	} else {
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+		bookmarks.push(bookmark);
+
+		console.log(bookmarks);
+
+		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	}
+
+	document.getElementById('myForm').reset();
+}
+
+function fetchBookmarks(){
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+	var bookmarksResults = document.getElementById('bookmarksResults');
+
+	bookmarksResults.innerHTML = '';
+
+	for(var i = 0; i < bookmarks.length; i++){
+		var name = bookmarks[i].name;
+		var url = bookmarks[i].url;
+
+		bookmarksResults.innerHTML += '<div class="well">'+
+																	'<h3>'+name+
+																	'</h3>'+
+																	'</div>'
+	}
 }
